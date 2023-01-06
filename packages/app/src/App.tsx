44b68c3992f5e6ad77e5796057056c99a77ff58b
frodrigues <fagner.squadp@genial.com.vc>
2023-01-06 11:13:44 -0300

    feat: initial commit

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import App2 from '@monorepo/payment'
+
+const App = () => {
+  return (
+    <>
+      <h1>MONOREPO APP</h1>
+      <App2 />
+    </>
+  )
+}
+
+export default App
---
+++ packages/payment/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return <h2>AQUI VAI O MODULO DE PAGAMENTOS</h2>
+}
+
+export default App
